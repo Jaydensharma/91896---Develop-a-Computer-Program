74097651f05758e16613ae8a85f043a2eb960fab
Jaydensharma <21057@students.lynfield.school.nz>
2024-08-14 21:01:29 +1200

    Update FinalProgram.py
    Loaded my Prototype Stage 3 File in from my computer which is the start of me creating the main code

--- FinalProgram.py
+++ FinalProgram.py
@@ -1,1 +1,30 @@
+#03/08/2024
+#Author: Jayden Sharma
+#Purpose: The purpose of this stage is to figure out how to make the main menu
 
+
+import tkinter as tk
+
+class Application(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Julie's Party Hire")
+        self.geometry("400x300")
+
+        # Create buttons
+        tk.Button(self, text="Hire", command=self.open_hire_window).pack(pady=10)
+        tk.Button(self, text="Return", command=self.open_return_window).pack(pady=10)
+        tk.Button(self, text="Quit", command=self.quit_application).pack(pady=10)
+
+    def open_hire_window(self):
+        pass  # To be implemented
+
+    def open_return_window(self):
+        pass  # To be implemented
+
+    def quit_application(self):
+        self.destroy()
+
+if __name__ == "__main__":
+    app = Application()
+    app.mainloop()
