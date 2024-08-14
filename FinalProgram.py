12cc6b6eb3bf0f0e496f15693c47433e562bb8d0
Jaydensharma <21057@students.lynfield.school.nz>
2024-08-14 21:17:29 +1200

    Update FinalProgram.py
    Loaded my fourth prototype that got me to my final program

--- FinalProgram.py
+++ FinalProgram.py
@@ -1,34 +1,44 @@
-#03/08/2024
+#05/08/2024
 #Author: Jayden Sharma
-#Purpose: The purpose of this stage is to figure out how to make the main menu
+#Purpose: The purpose of this stage is to figure start creating the hire window to start setting everything up
 
-#Imports tkinter
+#imports tkinter
 import tkinter as tk
+from tkinter import *
 
-#Creates the application and names it
+#creates the application as a class
 class Application(tk.Tk):
     def __init__(self):
         super().__init__()
-        #names the window Julie's Party Hire
+        #names the window
         self.title("Julie's Party Hire")
-        #creates the size of the window
+        #sizes the window
         self.geometry("400x300")
 
-        # Create buttons of the main menu
-        tk.Button(self, text="Hire", command=self.open_hire_window).pack(pady=10)
-        tk.Button(self, text="Return", command=self.open_return_window).pack(pady=10)
-        tk.Button(self, text="Quit", command=self.quit_application).pack(pady=10)
+        #creates the buttons
+        Button(self, text="Hire", command=self.open_hire_window).pack(pady=10)
+        Button(self, text="Return", command=self.open_return_window).pack(pady=10)
+        Button(self, text="Quit", command=self.quit_application).pack(pady=10)
 
     def open_hire_window(self):
-        pass  # To be implemented
+        HireWindow(self) #sends the user to the hire window
 
     def open_return_window(self):
-        pass  # To be implemented
+        pass #doesnt do anything at this stage
 
     def quit_application(self):
+        #ends the program which is also the quit button
         self.destroy()
 
-#loops the main menu so it doesnt just close and keeps on refreashing until the user inputs something
+#puts the hire window in its own class and own application
+class HireWindow(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        #names and sizes the hire window
+        self.title("Hire Window")
+        self.geometry("300x200")
+
+#makes the application and loops it until the user inputs
 if __name__ == "__main__":
     app = Application()
     app.mainloop()
